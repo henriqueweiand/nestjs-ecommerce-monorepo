8ab78252fe26b0601a1e8800584e293ce00cda9b
henrique weiand <henriqueweiand@gmail.com>
2024-04-06 17:47:09 -0300

    base configs

--- apps/auth/src/users/users.module.ts
+++ apps/auth/src/users/users.module.ts
@@ -3,9 +3,18 @@ import { DatabaseModule, User } from '@app/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersRepository } from './users.repository';
+import { PersistenceModule } from '@app/common/persistence/persistence.module';
 
 @Module({
-  imports: [DatabaseModule, DatabaseModule.forFeature([User])],
+  imports: [
+    // DatabaseModule,
+    // DatabaseModule.forFeature([User]),
+    PersistenceModule.forDatabase({
+      models: [User],
+      module: 'auth',
+    }),
+    PersistenceModule.forFeature([User]),
+  ],
   controllers: [UsersController],
   providers: [UsersService, UsersRepository],
   exports: [UsersService],
